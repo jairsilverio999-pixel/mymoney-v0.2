509de2b3b00c9fc15b48b774e35d1cd919860f18
Jair Silverio <jair.silverio999@gmail.com>
2026-04-04 00:42:40 -0600

    Formulario gastos

---
+++ script/inputGast.js
@@ -0,0 +1,18 @@
+let gast = [];
+let categories = [];
+let cards = [];
+
+//Se registra el monto
+let totalGast = 0
+function ingGast(){
+    const gast = parseFloat(document.getElementById("montGast").value);
+    totalGast += gast;
+    return totalGast;
+}
+
+function regCategories(params) {
+    const categories = 
+    
+}
+
+
