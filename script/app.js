c456a4b60fdef01c3ff35c32d64fdb0d95f6c31a
Jair Silverio <jair.silverio999@gmail.com>
2026-04-10 12:36:38 -0600

    Lista de gastos (avanze, desmenuzar)

--- script/app.js
+++ script/app.js
@@ -1,18 +1,11 @@
-
-let gastosSaved = []
 const form = document.getElementById('dataRegister')
-//localStorage.setItem()
+const lista = document.getElementById('allExpensives')
+const totalExpensives = document.getElementById('totalExpensive')
+
 let gastos = JSON.parse(localStorage.getItem('gasto')) || []
+render()
 
-// Guarda el valor del gasto
-function regGasto(){     
-    const monto = document.getElementById('montGast').value
-    localStorage.setItem('gasto', JSON.stringify(monto))
-    let gastosSaved = JSON.parse(localStorage.getItem('gasto')) || [] // si lo de la derecha es falsy, usa una lista vacia como dato a almacenar
-    console.log(monto)
-}
-
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function(e){ // guarda los gastos en localstore
     e.defaultPrevented
     alert('Formulario detenido')
     let expenditure = parseFloat(document.getElementById('montGast').value)
@@ -23,11 +16,25 @@ form.addEventListener('submit', function(e){
     console.log(card)
     let categorie = document.getElementById('categories').value
     console.log(categorie)
-    
     gastos.push({expenditure, description, card, categorie})
     localStorage.setItem('gasto', JSON.stringify(gastos))
     console.log(gastos)
+    render()
+    form.reset()
 
 })
+function render() { //Actualiza la lista de gastos
+    lista.innerHTML = ''
+    let gastoTotal = 0
+    gastos.forEach((gasto, index) => {
+        console.log(gasto, index)
+        let li = document.createElement('li')
+        li.textContent = `Monto: $${gasto.expenditure}, Descripcion: ${gasto.description}, Origen: ${gasto.card}, Categoria: ${gasto.categorie}`
+        lista.appendChild(li)
+        gastoTotal += gasto.expenditure
+    });
+    totalExpensives.textContent = gastoTotal
+}
 console.log(gastos)
+
 
