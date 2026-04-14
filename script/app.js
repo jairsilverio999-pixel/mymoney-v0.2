06a56e4312ded62882142c4426c938752399fec8
Jair Silverio <jair.silverio999@gmail.com>
2026-04-14 01:40:14 -0600

    CSS gastos, btn borrar gasto por id

--- script/app.js
+++ script/app.js
@@ -4,11 +4,13 @@ const totalExpensives = document.getElementById('totalExpensive')
 const reset = document.getElementById('reset')
 
 let gastos = JSON.parse(localStorage.getItem('gasto')) || []
+let id = typeof gastos.id
 render()
 
 form.addEventListener('submit', function(e){ // guarda los gastos en localstore
     e.defaultPrevented
     alert('Formulario detenido')
+    let id = Date.now()
     let expenditure = parseFloat(document.getElementById('montGast').value)
     console.log(expenditure)
     let description = document.getElementById('description').value
@@ -17,35 +19,53 @@ form.addEventListener('submit', function(e){ // guarda los gastos en localstore
     console.log(card)
     let categorie = document.getElementById('categories').value
     console.log(categorie)
-    gastos.push({expenditure, description, card, categorie})
+    gastos.push({id, expenditure, description, card, categorie})
     localStorage.setItem('gasto', JSON.stringify(gastos))
     console.log(gastos)
     render()
     form.reset()
 
 })
+
 function render() { //Actualiza la lista de gastos y los agrega en forma de lista
     lista.innerHTML = ''
     let gastoTotal = 0
-    gastos.forEach((gasto, index) => {
-        console.log(gasto, index)
+    gastos.forEach((gasto) => {
         let li = document.createElement('li')
         li.textContent = `$${gasto.expenditure}, Descripcion:  ${gasto.description}, Origen: ${gasto.card}, Categoria: ${gasto.categorie}`
+        let btn = document.createElement('button')
+        btn.textContent = 'Eliminar gasto'
+        btn.addEventListener('click', function () {
+            deleteExpensive(gasto.id)
+        })
+        li.appendChild(btn)
         lista.appendChild(li)
+        
         gastoTotal += gasto.expenditure
     });
     totalExpensives.textContent = gastoTotal
 }
-console.log(gastos)
+
+//borrar gasto por id
+function deleteExpensive(id) {
+    gastos = gastos.filter(function (gasto){
+        console.log(gasto.id)
+        return gasto.id !== id
+    })
+    localStorage.setItem('gasto', JSON.stringify(gastos))
+    render()
+}
 
 // borra la lista de gastos
 reset.addEventListener("click", function(){
     if(confirm('Seguro que desea eliminar todos los gastos?')){
         localStorage.clear();
-    location.reload()
+        lista.innerHTML = ''
+        totalExpensives.textContent = ''
     }
 })
 
+// cambio de pantalla menu
 function changeScreen(id){
     const sections = document.querySelectorAll('.screen')
     sections.forEach(section => {
@@ -54,4 +74,5 @@ function changeScreen(id){
     document.getElementById(id).classList.remove('hide')
 }
 
+console.log(id)
 
