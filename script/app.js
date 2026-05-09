3711bae1351cf44ce32b88eec982d5a28f580ac6
Jair Silverio <jair.silverio999@gmail.com>
2026-05-09 10:13:43 -0600

    systemEdit

--- script/app.js
+++ script/app.js
@@ -8,9 +8,12 @@ const listCardsSaved = document.getElementById('listCardsSaved')
 const formCategories = document.getElementById('formCategories')
 const listCategoriesSaved = document.getElementById('listCategoriesSaved')
 const selectCategories = document.getElementById('categories')
+let idEditData = null
 
+// objetos
 let gastos = JSON.parse(localStorage.getItem('gasto')) || []
 let cards = JSON.parse(localStorage.getItem('card'))  || []
+console.log(cards)
 let categories = JSON.parse(localStorage.getItem('categorie')) || []
 
 updateUI()
@@ -27,11 +30,23 @@ formExpensives.addEventListener('submit', function(e){
     console.log(card)
     let categorie = document.getElementById('categories').value
     console.log(categorie)
-    gastos.push({id, expenditure, description, card, categorie})
+    if (idEditData) {
+        gastos = gastos.map(gasto =>{
+            if (gasto.id === idEditData) {
+                return {...gasto, id:idEditData, expenditure, description, card, categorie}
+            }
+            return card
+        })
+        idEditData = null
+    }
+    else{
+        gastos.push({id, expenditure, description, card, categorie})
+    }
     localStorage.setItem('gasto', JSON.stringify(gastos))
     console.log(gastos)
     render()
     formExpensives.reset()
+    formExpensives.querySelector('button').textContent = 'Registrar gasto'
     alert('Gasto registrado con exito')
 })
 
@@ -76,11 +91,23 @@ formCards.addEventListener("submit", function(e){
         return
     }
     let typeCard = document.getElementById('typeCard').value
-    cards.push({id, alias, bank, noCard, typeCard})
+    if (idEditData) {
+        cards = cards.map(card =>{
+            if (card.id === idEditData) {
+                return {...card, id:idEditData, alias, bank, noCard, typeCard}
+            }
+            return card
+        })
+        idEditData = null
+    }
+    else{
+        cards.push({id, alias, bank, noCard, typeCard})
+    }
     localStorage.setItem('card', JSON.stringify(cards))
     createList(cards, listCardsSaved, 'card')
     menuBanks()
     formCards.reset()
+    formCards.querySelector('button').textContent = 'Registrar tarjeta'
     alert('Tarjeta registrada correctamente')
 })
 
@@ -104,11 +131,24 @@ formCategories.addEventListener('submit', function(e){
     let id = Date.now()
     let category = document.getElementById('categorie').value
     console.log(category)
-    categories.push({id, category})
+    console.log(idEditData)
+    if (idEditData) {
+        categories = categories.map( categorie =>{
+            if (categorie.id === idEditData) {
+                return {id:idEditData, category}
+            }
+            return categorie
+        })
+        idEditData = null
+    }
+    else{
+        categories.push({id, category})
+    }
     localStorage.setItem('categorie', JSON.stringify(categories))
     createList(categories, listCategoriesSaved, 'categorie')
     menuCategories()
     formCategories.reset()
+    formCategories.querySelector('button').textContent = 'Registrar categoria'
     alert('Categoria registrada exitosamente')
 })
 
@@ -137,13 +177,23 @@ function createList(saveInLocalstore, list, keyObj){
             }
             valuesList += `${obj[key]} - `
         }
-        li.textContent = valuesList.slice(0 , -3)
-        let btn = document.createElement('button')
-        btn.textContent = 'Eliminar'
-        btn.addEventListener('click', function () {
+        let leftSpan = document.createElement('span')
+        leftSpan.textContent = valuesList.slice(0 , -3)
+        let divBtnEditDelete = document.createElement('div')
+        let btnEdit = document.createElement('button')
+        btnEdit.textContent = 'Editar'
+        btnEdit.addEventListener('click', function(){
+            editDataRegister(obj, keyObj)
+        })
+        let btnDelete = document.createElement('button')
+        btnDelete.textContent = 'Eliminar'
+        btnDelete.addEventListener('click', function () {
             deleteByID(obj.id, saveInLocalstore, keyObj)
         })
-        li.appendChild(btn)
+        divBtnEditDelete.appendChild(btnEdit)
+        divBtnEditDelete.appendChild(btnDelete)
+        li.appendChild(leftSpan)
+        li.appendChild(divBtnEditDelete)
         list.appendChild(li)
     });
 }
@@ -165,4 +215,32 @@ function deleteByID(id, arrayInLocalStore, key) {
     const newArrayInLocalStore = arrayInLocalStore.filter(obj => obj.id !== id)
     localStorage.setItem(key, JSON.stringify(newArrayInLocalStore))
     updateUI()
+}
+
+// editor de objetos (a futuro mejor un objeto)
+function editDataRegister(objet, typeDataSaved){
+    console.log(objet.id)
+    idEditData = objet.id
+    switch (typeDataSaved) {
+        case 'gasto':
+            document.getElementById('montGast').value = objet.expenditure
+            document.getElementById('description').value = objet.description
+            document.getElementById('cards').value = objet.card
+            document.getElementById('categories').value = objet.categorie   
+            formExpensives.querySelector('button').textContent = 'Actualizar gasto'
+            break;
+        case 'card':
+            document.getElementById('aliasCard').value = objet.alias
+            document.getElementById('bank').value = objet.bank
+            document.getElementById('noCard').value = objet.noCard
+            document.getElementById('typeCard').value = objet.typeCard
+            formCards.querySelector('button').textContent = 'Actualizar gasto'
+        case 'categorie':
+            document.getElementById('categorie').value = objet.category 
+            formCategories.querySelector('button').textContent = 'Actualizar gasto'
+        default:
+            console.log(idEditData)
+            break;
+    }
+    console.log('Editando')
 }
